e36099f2c76cf58fe8d105584b087b3793c17acd
Reid Draper <reiddraper@gmail.com>
2016-03-18 10:12:29 -0600

    More resilient parsing
    We can now parse lines like:
    
    Main.main.stateRunner (src/Main.hs:74:7-45)
    
    Which span only a single line. We also don't fail the entire parser if
    one stack frame line fails to parse.

--- src/Rollover.hs
+++ src/Rollover.hs
@@ -15,6 +15,7 @@ module Rollover
     , recordException
     ) where
 
+import Control.Applicative ((<|>))
 import Control.Concurrent.Async (Async, async)
 import Control.Monad (void)
 import Control.Exception (SomeException(..), displayException)
@@ -102,22 +103,44 @@ exceptionInfo (SomeException e) =
     where eType = (pack . show . typeRep . asProxyTypeOf) e
           eDesc = pack (displayException e)
 
+int :: Parser Int
+int = decimal
+
+moduleAndFunctionParser :: Parser Text
+moduleAndFunctionParser = takeWhile1 (/= ' ')
+
+fileNameParser :: Parser Text
+fileNameParser =
+    space >> char '(' >> takeWhile1 (/= ':') >> string ":"
+
+multiLineLocationParser :: Parser (Int, Int)
+multiLineLocationParser = do
+    _ <- string "("
+    lineNo <- int
+    void (char ',')
+    column <- int
+    void $ string ")-(" >> int >> char ',' >> int >> string ")"
+    return (lineNo, column)
+
+singleLineLocationParser :: Parser (Int, Int)
+singleLineLocationParser = do
+    lineNo <- int
+    _ <- string ":"
+    column <- int
+    _ <- string "-"
+    void int
+    return (lineNo, column)
+
 stackFrameParser :: Parser StackFrame
 stackFrameParser = do
-    let int :: Parser Int
-        int = decimal
 
-    moduleAndFunction <- takeWhile1 (/= ' ')
+    moduleAndFunction <- moduleAndFunctionParser
 
-    void $ space >> char '(' >> takeWhile1 (/= ':') >> string ":("
+    void fileNameParser
 
-    lineNo <- int
+    (lineNo, column) <- multiLineLocationParser <|> singleLineLocationParser
 
-    void (char ',')
-
-    column <- int
-
-    void $ string ")-(" >> int >> char ',' >> int >> string "))"
+    _ <- string ")"
 
     let splitUp = splitOn "." moduleAndFunction
         function = last splitUp
@@ -128,6 +151,13 @@ stackFrameParser = do
         , _lineNo = lineNo
         , _colNo = column
         }
+
+lenientStackFrameParse :: Text -> StackFrame
+lenientStackFrameParse frame =
+    case parseOnly stackFrameParser frame of
+        (Right f) -> f
+        (Left _) ->
+            StackFrame frame frame 0 0
 
 recordException
     :: ApiKey
@@ -140,5 +170,4 @@ recordException apiKey environment codeVersion exception stackTrace =
     async (void (post "https://api.rollbar.com/api/1/item/" (toJSON rbItem)))
     where rbItem = RollbarItem apiKey environment codeVersion exc
           exc = RollbarException (exceptionInfo exception) trace
-          trace = either (const []) id
-                    (sequence (parseOnly stackFrameParser . pack <$> stackTrace))
+          trace = lenientStackFrameParse . pack <$> stackTrace
